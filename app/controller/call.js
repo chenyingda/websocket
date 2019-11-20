d4e3c12d9936b85990e87d27da1b1cbc23956cba
chenyingda <chenyingda@yeezon.com>
2019-11-20 17:44:06 +0800

    fix: 服务端代码修改, 测试wss成功

--- app/controller/call.js
+++ app/controller/call.js
@@ -4,9 +4,17 @@ const WebSocket = require('ws');
 module.exports = app => {
   return class callController extends app.Controller {
     async index() {
-      const ws = new WebSocket('wss://cydxbc123.natapp1.cc');
-      ws.on('open', function open() {
-        this.ctx.logger.info('socket connected');
+      // 这里的由于使用的是wss/ws协议, 并非用的是http协议, 所以当前socket一直保持连接, 如果没有一方主动断开的话, ws会一直监听message函数
+      // 只要接收到来自客户端的信息, 服务端就会自动响应
+      const wss = new WebSocket.Server({ server: this.app.server });
+      wss.on('connection', function(ws) {
+        ws.on('message', function(message) {
+          ws.send(`get the message ${message}`);
+          if (message.indexOf('method') > -1) {
+            ws.close();
+            console.log('socket close');
+          }
+        });
       });
     }
   };
--- app/controller/home.js
+++ app/controller/home.js
@@ -2,15 +2,11 @@
 ;
 const Promise = require('bluebird');
 const skus_arr = require('../util/sku');
-console.log('nmsl');
 module.exports = app => {
-  console.log('xxxxx', app);
   class HomeController extends app.Controller {
     async index() {
       const { ctx } = this;
-      console.log('aaa');
       const { Sku, Spu } = ctx.model;
-      console.log('bbb');
       // const result = await app.model.query('select * from sku where sku_id = 5264325');
       // const result = await app.model.query("SELECT `sku`.`sku_id`, `sku`.`product_id`, `sku`.`meta_id`, `sku`.`image_src`, `sku`.`options`, `sku`.`price`, `sku`.`stock`, `sku`.`cb_name`, `sku`.`cb_unit`, `sku`.`cb_code`, `sku`.`cb_barcode`, `sku`.`cb_taxrate`, `sku`.`cb_country`, `sku`.`cb_warehouse`, `sku`.`created_at`, `spu`.`product_id` AS `spu.product_id`, `spu`.`meta_id` AS `spu.meta_id`, `spu`.`image_src` AS `spu.image_src`, `spu`.`handle` AS `spu.handle`, `spu`.`name` AS `spu.name`, `spu`.`brand_name` AS `spu.brand_name`, `spu`.`types` AS `spu.types`, `spu`.`visibility` AS `spu.visibility`, `spu`.`created_at` AS `spu.created_at` FROM `sku` AS `sku` LEFT OUTER JOIN `spu` AS `spu` ON `sku`.`product_id` = `spu`.`product_id` AND (`spu`.`deleted_at` > '2019-11-07 15:33:52' OR `spu`.`deleted_at` IS NULL) WHERE ((`sku`.`deleted_at` > '2019-11-07 15:33:52' OR `sku`.`deleted_at` IS NULL) AND `sku`.`sku_id` = 5264325)");
       const result = await Sku.findAll({
